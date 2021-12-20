baf58db45b54411ced33b55a74e1b0970c4eaecb
Kamil Skomro <kamil.skomro@comarch.com>
2021-12-20 22:19:56 +0100

    add drawing legend

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import pygame
-
-import model.jsonHelper
+from drawer.draw_floor import draw_floor
+from drawer.legend_drawer import LegendDrawer
 from drawer.rectangle import Rectangle
 from drawer.room_drawer import RoomDrawer
-from graph.draw_graph import draw_floor
 from model.jsonHelper import fromFile
 
 floor0 = fromFile("floor0.json")
 
 rect_size = 25
 pygame.init()
-screen = pygame.display.set_mode((1500, 600))
+screen = pygame.display.set_mode((1500, 800))
 running = True
 color_red = (255, 0, 0)
 color_green = (0, 255, 0)
 rooms = floor0.get_all_rooms()
 room_drawer = RoomDrawer(
     screen,
-    Rectangle(0, 0, screen.get_width(), screen.get_height()),
+    Rectangle(0, 0, 1500, 600),
     border_width=2,
     blocks_x=floor0.get_blocks_x(),
     blocks_y=floor0.get_blocks_y()
 )
 draw_floor(floor0, room_drawer)
+legend_drawer = LegendDrawer(screen, Rectangle(0, 600, 1500, 200))
+legend_drawer.draw()
 font = pygame.font.SysFont('Arial', 12)
 BLACK = (0, 0, 0)
 step = 0
